6c3615797bbe6a5018270e6ab3988c476d72e114
Sebastian Rueth <sebastian.rueth@protonmail.com>
2021-12-02 17:35:21 +0100

    Day 02 Part 02

--- src/day_02.rs
+++ src/day_02.rs
@@ -3,12 +3,13 @@ use crate::assets::ASSETS_FOLDER;
 struct Submarine {
     depth: i32,
     horizontal_position: i32,
+    aim: i32,
 }
 
 
 pub fn run() {
-    part_01();
-    // part_02();
+    // part_01();
+    part_02();
 }
 
 
@@ -20,14 +21,24 @@ fn part_01() {
     let mut my_sub = Submarine::new();
 
     for line in lines {
-        my_sub.handle_command(line);
+        my_sub.handle_command_01(line);
     }
 
     println!("02/01: Horizontal speed and depth multiplied: {}", my_sub.horizontal_position * my_sub.depth);
 }
 
 fn part_02() {
+    let bytes = ASSETS_FOLDER.get_file("day02part02.input").unwrap().contents();
+    let string = String::from_utf8_lossy(bytes);
+    let mut lines = string.lines();
 
+    let mut my_sub = Submarine::new();
+
+    for line in lines {
+        my_sub.handle_command_02(line);
+    }
+
+    println!("02/02: Horizontal speed and depth multiplie: {}", my_sub.horizontal_position * my_sub.depth);
 }
 
 
@@ -36,10 +47,33 @@ impl Submarine {
         Submarine {
             depth: 0,
             horizontal_position: 0,
+            aim: 0,
         }
     }
 
-    pub fn handle_command(&mut self, cmd: &str) {
+    pub fn handle_command_02(&mut self, cmd: &str) {
+        let split_cmd: Vec<&str> = cmd.trim().split(" ").collect();
+        let command = *split_cmd.get(0).unwrap();
+        let num: i32 = split_cmd.get(1).unwrap().parse().unwrap();
+
+        match command {
+            "forward" => {
+                self.horizontal_position += num;
+                self.depth += self.aim * num;
+            },
+            "up" => {
+                self.aim -= num;
+            },
+            "down" => {
+                self.aim += num;
+            }
+            _ => {
+                panic!("Unrecognized input: {:?}", cmd);
+            }
+        }
+    }
+
+    pub fn handle_command_01(&mut self, cmd: &str) {
         let split_cmd: Vec<&str> = cmd.trim().split(" ").collect();
         let command = *split_cmd.get(0).unwrap();
         let num: i32 = split_cmd.get(1).unwrap().parse().unwrap();
@@ -54,7 +88,7 @@ impl Submarine {
             "up" => {
                 self.depth -= num;
             },
-            _ => panic!("IDK: {:?}", cmd),
+            _ => panic!("Unrecognized input: {:?}", cmd),
         }
     }
 }
