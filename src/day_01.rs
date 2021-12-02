41d2435ffc519c7c4e0475b3f7b04915ac5cc456
Sebastian Rueth <sebastian.rueth@protonmail.com>
2021-12-02 17:13:11 +0100

    Day 01 Part 02

--- src/day_01.rs
+++ src/day_01.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 use crate::assets::ASSETS_FOLDER;
 
 pub fn run() {
@@ -12,10 +13,10 @@ fn part_01() {
 
     let mut num_positive_depth_change = 0;
 
-    let mut last_value = lines.next().unwrap().parse::<i32>().unwrap();
+    let mut last_value = lines.next().unwrap().parse::<u32>().unwrap();
 
-    for (i, e) in lines.enumerate() {
-        let cur_value: i32 = e.parse().unwrap();
+    for line in lines {
+        let cur_value: u32 = line.parse().unwrap();
 
         if cur_value > last_value {
             num_positive_depth_change += 1;
@@ -29,4 +30,34 @@ fn part_01() {
 
 fn part_02() {
     let bytes = ASSETS_FOLDER.get_file("day01part02.input").unwrap().contents();
+    let string = String::from_utf8_lossy(bytes);
+    let mut lines = string.lines();
+
+    let mut num_positive_depth_change = 0;
+    let mut last_sum = lines.clone().take(3).map(|i| i.parse::<u32>().unwrap()).sum();
+
+    let mut values = VecDeque::new();
+
+    for line in lines {
+        values.push_back(line.parse::<u32>().unwrap());
+
+        // If the Queu doesn't have 3 values yet, continue to next loop iteration
+        if values.len() < 3 {
+            continue;
+        }
+
+
+        let sum: u32 = values.iter().sum();
+
+        // Discard popped value
+        let _ = values.pop_front();
+
+        if sum > last_sum {
+            num_positive_depth_change += 1;
+        }
+
+        last_sum = sum;
+    }
+
+    println!("01/02: Number of depth increases: {}", num_positive_depth_change);
 }
