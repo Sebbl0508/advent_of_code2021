f8a81e6f325d3932761a8482f863a426f8ed7461
Sebastian Rüth <sebastian.rueth@protonmail.com>
2021-12-04 16:54:52 +0100

    Added Day 04

---
+++ src/day_04.rs
@@ -0,0 +1,12 @@
+pub fn run() {
+    part_01();
+    // part_02();
+}
+
+fn part_01() {
+    todo!();
+}
+
+fn part_02() {
+    todo!();
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod assets;
 mod day_01;
 mod day_02;
 mod day_03;
+mod day_04;
 
 fn main() {
     day_01::run();
