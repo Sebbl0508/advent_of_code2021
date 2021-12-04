19e031b90f0802b9c27bcd03d2b9f361bc4d5f0d
Sebastian Rüth <sebastian.rueth@protonmail.com>
2021-12-04 23:01:04 +0100

    Day 04 Part 02 Ugly code :(

--- src/day_04.rs
+++ src/day_04.rs
@@ -1,5 +1,5 @@
-use std::str::Lines;
 use crate::assets::ASSETS_FOLDER;
+use std::str::Lines;
 
 #[derive(Debug, Clone)]
 struct Board {
@@ -12,7 +12,7 @@ struct BoardMgr {
 }
 
 pub fn run() {
-    // part_01();
+    part_01();
     part_02();
 }
 
@@ -21,43 +21,88 @@ fn part_01() {
     let string = String::from_utf8_lossy(bytes).to_string();
 
     let mut lines = string.lines();
-    let numbers: Vec<u32> = lines.next().unwrap().split(",").map(|x| x.parse::<u32>().unwrap()).collect();
+    let numbers: Vec<u32> = lines
+        .next()
+        .unwrap()
+        .split(",")
+        .map(|x| x.parse::<u32>().unwrap())
+        .collect();
 
     let mut board_mgr = BoardMgr::new(&mut lines);
 
     'top_loop: for i in numbers {
         board_mgr.add_drawn_nums(i);
 
-        for board in &board_mgr.boards {
+        for board in board_mgr.boards.iter() {
             if board.has_bingo(&board_mgr.drawn_nums) {
-                println!("04/01: Sum of non-drawn numbers * current drawn number: {}", board.sum_of_unmarked(&board_mgr.drawn_nums) * i);
+                println!(
+                    "04/01: Sum of non-drawn numbers * current drawn number: {}",
+                    board.sum_of_unmarked(&board_mgr.drawn_nums) * i
+                );
                 break 'top_loop;
             }
         }
     }
-
-
 }
 
 fn part_02() {
-    todo!();
+    let bytes = ASSETS_FOLDER.get_file("day04.input").unwrap().contents();
+    let string = String::from_utf8_lossy(bytes).to_string();
+
+    let mut lines = string.lines();
+    let numbers: Vec<u32> = lines
+        .next()
+        .unwrap()
+        .split(",")
+        .map(|x| x.parse::<u32>().unwrap())
+        .collect();
+
+    let mut board_mgr = BoardMgr::new(&mut lines);
+    let mut boards_won = Vec::new();
+    let mut last_drawn_num = 0;
+
+    'top_loop: for num in numbers {
+        board_mgr.add_drawn_nums(num);
+
+        for (i, board) in board_mgr.boards.iter().enumerate() {
+            if board.has_bingo(&board_mgr.drawn_nums) {
+                if boards_won.contains(&i) {
+                    continue;
+                }
+                boards_won.push(i);
+                last_drawn_num = num;
+                if boards_won.len() == board_mgr.boards.len() {
+                    break 'top_loop;
+                }
+            }
+        }
+    }
+    let sum_of_unmarked = board_mgr
+        .boards
+        .get(*boards_won.last().unwrap())
+        .unwrap()
+        .sum_of_unmarked(&board_mgr.drawn_nums);
+
+    println!(
+        "04/02: Sum of non-drawn numbers * current drawn number: {}",
+        sum_of_unmarked * last_drawn_num
+    );
 }
-
-
 
 impl Board {
     pub fn new(lines: Vec<&str>) -> Self {
         let mut intern = Vec::new();
 
         for line in lines {
-            let numbers: Vec<u32> = line.split(" ").filter(|&x| x != "").map(|x| x.parse::<u32>().unwrap()).collect();
+            let numbers: Vec<u32> = line
+                .split(" ")
+                .filter(|&x| x != "")
+                .map(|x| x.parse::<u32>().unwrap())
+                .collect();
             intern.push(numbers);
         }
 
-
-        Self {
-            num_board: intern,
-        }
+        Self { num_board: intern }
     }
 
     pub fn sum_of_unmarked(&self, nums_coollected: &Vec<u32>) -> u32 {
@@ -74,12 +119,8 @@ impl Board {
     }
 
     pub fn has_bingo(&self, nums_collected: &Vec<u32>) -> bool {
-        let mut col = Vec::new();
-
         for row in self.num_board.iter() {
             let mut cnt = 0;
-
-            col.push(row[0]);
 
             for i in row {
                 if nums_collected.contains(i) {
@@ -92,9 +133,21 @@ impl Board {
             }
         }
 
-        assert_eq!(col.len(), 5);
+        for col_idx in 0..self.num_board.len() {
+            let mut cnt = 0;
 
-        return col.iter().all(|x| nums_collected.contains(x));
+            for row_idx in 0..self.num_board.len() {
+                if nums_collected.contains(&self.num_board[row_idx][col_idx]) {
+                    cnt += 1;
+                }
+            }
+
+            if cnt == self.num_board.len() {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /*
