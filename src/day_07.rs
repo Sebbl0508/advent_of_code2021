9aaf631ce91fa178ae14700e62e40d21848d406c
Sebastian Rueth <sebastian.rueth@protonmail.com>
2021-12-07 12:48:06 +0100

    Info what to do (mayb)

--- src/day_07.rs
+++ src/day_07.rs
@@ -3,3 +3,18 @@ use crate::assets::ASSETS_FOLDER;
 pub fn run() {
     todo!();
 }
+
+// Binary Search?
+// Begin at average of all values (sum / len_of_vec)
+// Get the fuel consumption
+// Get the average of both halfes again now (1/4 & 3/4)
+// Get fuel consumption of both halfes
+// Continue wherever the fuel consumption is lower than the first
+fn part_01(input: &String) {
+    let starting_positions: Vec<&str> = input.iter().split(",").collect();
+}
+
+
+fn part_02(input: &String) {
+    todo!();
+}
