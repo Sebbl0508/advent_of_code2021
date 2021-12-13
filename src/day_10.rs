4bf162dbdeefb1e20c73e72b3226259abb4c21ee
Sebastian Rüth <sebastian.rueth@protonmail.com>
2021-12-13 12:19:39 +0100

    Remove unused

--- src/day_10.rs
+++ src/day_10.rs
@@ -1,31 +1,11 @@
 use crate::assets::ASSETS_FOLDER;
 use itertools::Itertools;
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-enum ChunkType {
-    Round,  // ()
-    Square, // []
-    Curly,  // {}
-    Pointy, // <>
-}
-
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-enum ChunkPart {
-    Opening,
-    Closing,
-}
-
-#[derive(Debug, Clone, Copy, Eq, PartialEq)]
-struct Part {
-    part: ChunkPart,
-    typ: ChunkType,
-}
-
 pub fn run() {
     let bytes = ASSETS_FOLDER.get_file("day10.input").unwrap().contents();
     let string = String::from_utf8(bytes.to_vec()).unwrap();
 
-    // part_01(&string);
+    part_01(&string);
     part_02(&string);
 }
 
@@ -121,50 +101,4 @@ fn syntax_error_score_of_line(line: &str) -> u32 {
     }
 
     0
-}
-
-impl Part {
-    pub fn new(inp_char: char) -> Self {
-        let chunk_type = ChunkType::from_char(inp_char);
-        let chunk_part = match inp_char {
-            '(' | '[' | '{' | '<' => ChunkPart::Opening,
-            ')' | ']' | '}' | '>' => ChunkPart::Closing,
-            invalid => panic!("Invalid char: {:?}", invalid),
-        };
-
-        Self {
-            part: chunk_part,
-            typ: chunk_type,
-        }
-    }
-}
-
-impl ChunkType {
-    fn get_opening(&self) -> char {
-        match self {
-            ChunkType::Round => '(',
-            ChunkType::Square => '[',
-            ChunkType::Curly => '{',
-            ChunkType::Pointy => '<',
-        }
-    }
-
-    fn get_closing(&self) -> char {
-        match self {
-            ChunkType::Round => ')',
-            ChunkType::Square => ']',
-            ChunkType::Curly => '}',
-            ChunkType::Pointy => '>',
-        }
-    }
-
-    fn from_char(c: char) -> ChunkType {
-        match c {
-            '(' | ')' => ChunkType::Round,
-            '[' | ']' => ChunkType::Square,
-            '{' | '}' => ChunkType::Curly,
-            '<' | '>' => ChunkType::Pointy,
-            invalid => panic!("Invalid char: {:?}", invalid),
-        }
-    }
 }
